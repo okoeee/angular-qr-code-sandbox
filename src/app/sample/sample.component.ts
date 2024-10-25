dcad0d35ff040819b1fabdaa297c7637096bdc77
okoeee <yk27.puxcm@gmail.com>
2024-10-26 08:53:53 +0900

    add QR code generator

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {QRCodeModule} from 'angularx-qrcode';
 
 @Component({
   selector: 'sample',
   standalone: true,
   templateUrl: './sample.component.html',
   styleUrl: './sample.component.scss',
+  imports: [
+    QRCodeModule
+  ]
 })
 export class SampleComponent {}
